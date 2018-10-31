fd97b1b5c1b4f0322d2e707ea2b7252beb0e50c6
Jang Chanyong <zezs3535@naver.com>
2018-10-31 15:56:59 +0900

    function add in myas.h

--- Makefile
+++ Makefile
@@ -1,2 +1,12 @@
 # Write this Makefile
 
+main.o:
+	gcc -c main.c
+
+trans.o:
+	gcc -c trans.c
+
+check.o:
+	gcc -c check.c
+
+
--- myas.h
+++ myas.h
@@ -3,3 +3,9 @@
 #include<stdlib.h>
 
 // complete this header file
+
+int is_valid(char *op, char *args);
+
+int instr_trans(char *op, char *args, char *mcode);
+
+
