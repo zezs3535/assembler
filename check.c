c28ef78e355df33a7914abb797fb0116805643cc
Jang Chanyong <zezs3535@naver.com>
2018-11-13 15:43:08 +0900

    trans.c make

--- check.c
+++ check.c
@@ -8,17 +8,13 @@ int is_valid(char *op, char *args)
 	int j=0;
 	char *tok1, *tok2;
 	tok1=strtok(args,",");
-	while(i!='\0'){
-		i=tok1[j];
-		j++;
-	}
-	tok2=strtok(args+j,",");
+	tok2=strtok(NULL,",");
 	a=what_ins(tok1);
 	b=what_ins(tok2);
-	if((a==3||a==4)&&(b==3||b==4)){
+	if((a==3||a==4)&&(b==3||b==4)){ //mem to mem
 		return 0;
 	}
-	if(b==2){
+	if(b==2){ //dest!=immediate
 		return 0;
 	}
 	return 1;
--- trans.c
+++ trans.c
@@ -1,7 +1,6 @@
 #include "myas.h"
 
 int what_ins(char *ptr){
-	int i=0;
 	
 	if(ptr[0]=='%'){ //register
 		return 1;
@@ -9,10 +8,10 @@ int what_ins(char *ptr){
 	else if(ptr[0]=='$'){ //immediate
 		return 2;
 	}
-	else if(ptr[0]=='-'){ //memory
+	else if(ptr[0]=='-'){ //memory with disp.
 		return 3;
 	}
-	else if(ptr[0]=='0'){ //memory with disp.
+	else if(ptr[0]=='0'){ //memory
 		return 4;
 	}
 
@@ -23,6 +22,7 @@ int instr_trans(char *op, char *args, char* mcode)
 	char i;
 	int j=0;
 	int a,b;
+	char c;
 	char *tok1,*tok2;
 	// check syntax 
 	if(!is_valid(op, args)){
@@ -31,16 +31,32 @@ int instr_trans(char *op, char *args, char* mcode)
 	}
 	
 	tok1=strtok(args,",");
-	while(i!='\0'){
-		i=tok1[j];
-		j++;
-	}
-	tok2=strtok(args+j,",");
+	tok2=strtok(NULL,",");
 	a=what_ins(tok1);
 	b=what_ins(tok2);
-	strcpy(mcode, "AB CD EF");
-
-
-	
+	c=tok2[2];
+	if(b==1){
+		if(a==1){
+			strcpy(mcode,"89");
+		}
+		else if(a==2){
+			switch(c){
+				case'a':strcpy(mcode,"b8");
+					break;
+				case'b':strcpy(mcode,"bb");
+					break;
+				case'c':strcpy(mcode,"b9");
+					break;
+				case'd':strcpy(mcode,"ba");
+					break;
+			}
+		}
+		else if(a==3){
+			strcpy(mcode,"8b");
+		}
+		else if(a==4){
+			strcpy(mcode,"a1");
+		}
+	}
 	return 1;	
 }
