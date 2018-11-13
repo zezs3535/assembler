70014d1f17934171a3de7d041fd88b9de62db934
Jang Chanyong <zezs3535@naver.com>
2018-11-13 23:31:33 +0900

    complete

--- check.c
+++ check.c
@@ -3,14 +3,17 @@
 int is_valid(char *op, char *args)
 {
 	//if valid return 1, else 0
-
+	int i=0;
+	char c=0;
 	int a=0,b=0;
 	int j=0;
 	char *tok1, *tok2;
-	tok1=(char*)malloc(sizeof(char)*10);
-	tok2=(char*)malloc(sizeof(char)*10);
 	tok1=strtok(args,",");
-	tok2=strtok(NULL,",");
+	do{
+		c=tok1[i];
+		i++;
+	}while(c!='\0');
+	tok2=strtok(args+i,",");
 	a=what_ins(tok1);
 	b=what_ins(tok2);
 	if((a==3||a==4)&&(b==3||b==4)){ //mem to mem
--- trans.c
+++ trans.c
@@ -21,22 +21,28 @@ int what_ins(char *ptr){
 
 int instr_trans(char *op, char *args, char* mcode)
 {
-	
+	int i=0;	
 	int a=0,b=0;
 	char c=0;
-	char *tok1,*tok2;
+	char *arg1,*arg2;
+	char *tmp=(char*)malloc(sizeof(char)*20);
+
+	strcpy(tmp,args);
 	// check syntax 
 	if(!is_valid(op, args)){
+		strcpy(mcode,"error");
 		printf("Error: %s %s is not valid\n", op, args);
 		return 0;
 	}
-	tok1=(char*)malloc(sizeof(char)*10);
-	tok2=(char*)malloc(sizeof(char)*10);
-	tok1=strtok(args,",");
-	tok2=strtok(NULL,",");
-	a=what_ins(tok1);
-	b=what_ins(tok2);
-	c=tok2[2];
+	arg1=strtok(tmp,",");
+	do{
+		c=arg1[i];
+		i++;
+	}while(c!='\0');
+	arg2=strtok(tmp+i,",");
+	a=what_ins(arg1);
+	b=what_ins(arg2);
+	c=arg2[2];
 	if(b==1){
 		if(a==1){
 			strcpy(mcode,"89");
@@ -51,6 +57,8 @@ int instr_trans(char *op, char *args, char* mcode)
 					break;
 				case'd':strcpy(mcode,"ba");
 					break;
+				default:
+					return 0;
 			}
 		}
 		else if(a==3){
@@ -59,6 +67,10 @@ int instr_trans(char *op, char *args, char* mcode)
 		else if(a==4){
 			strcpy(mcode,"a1");
 		}
+		
+		
 	}
+	else
+		return 0;
 	return 1;	
 }
